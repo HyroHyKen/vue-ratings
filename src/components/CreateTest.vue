b671c81a5f8d87ab84782ad19a7aa567c45fec5a
Clergerie Dorian <doriancler@yahoo.fr>
2022-03-23 16:45:35 +0100

    createTest with criterion

--- src/components/CreateTest.vue
+++ src/components/CreateTest.vue
@@ -1,64 +1,116 @@
 <template>
 
   <input v-model="nameTest"
-         placeholder="Ajouter une evalution">
+         placeholder="nom de l'evaluation">
 
+
+  <input v-model="criterion"
+         placeholder="critère d'evaluation">
+
+  <button @click="addCriterion"> ajouter </button>
+  <ul v-if="criterions.length">
+    <li v-for="criter in criterions" :key="criter.id">
+      {{ criter }}
+      <button @click="removeCriterion(criter)"> supprimer</button>
+    </li>
+  </ul>
+  <p v-else>aucun critère</p>
+  <p v-if="!formOk" style="color: red; font-style: italic;">Veuillez entrer un nom ou des critères pour ajouter une évaluation</p>
+  <p v-if="!formOkCriterion" style="color: red; font-style: italic;">Veuillez entrer un nom pour ajouter un nouveau critère</p>
   <button @click="addNewTest"> valider </button>
 
 
 </template>
 
 <script>
-let db = '';
-let openRequest = indexedDB.open('db', 3);
-openRequest.onupgradeneeded = function(){
-  db = openRequest.result;
-  //Si l'objet de stockage users n'existe pas, on le crée
-  if (!db.objectStoreNames.contains('controle')){
-    db.createObjectStore('controle', {keyPath: 'id', autoIncrement: true});
-  }
-};
-openRequest.onerror = function(){
-  alert('Impossible d\'accéder à IndexedDB');
-};
-
-
-
 export default {
   name: "CreateTest",
   data() {
     return {
       nameTest: '',
+      criterion: '',
+      criterions: [],
+      formOkCriterion: true,
+      formOk: true
 
     }
   },
   methods: {
     addNewTest() {
-      db = openRequest.result;
-      let transaction = db.transaction('controle', 'readwrite');
+      if(this.nameTest && this.criterions.length) {
+        this.formOk = true;
+        let db = '';
+        let openRequest = indexedDB.open('db', 3);
+        openRequest.onupgradeneeded = function(){
+          db = openRequest.result;
+          //Si l'objet de stockage users n'existe pas, on le crée
+          if (!db.objectStoreNames.contains('controle')){
+            db.createObjectStore('controle', {keyPath: 'id', autoIncrement: true});
+          }
+        };
+        openRequest.onerror = function(){
+          alert('Impossible d\'accéder à IndexedDB');
+        };
 
-      let tests = transaction.objectStore('controle');
+        db = openRequest.result;
+        let transaction = db.transaction('controle', 'readwrite');
 
-      let test = {
-        name: this.nameTest,
-        notation : []
-      };
+        let tests = transaction.objectStore('controle');
 
-      let ajout = tests.add(test);
+        let test = {
+          name: this.nameTest,
+          notation: serialize(this.criterions)
+        };
+        console.log(test)
+        let ajout = tests.add(test);
 
-      ajout.onsuccess = function(){
-        alert('Evalution ajoutée');
-      };
-      ajout.onerror = function(){
-        alert('Erreur : ' + ajout.error);
-      };
+        ajout.onsuccess = function () {
+          alert('Evalution ajoutée');
+        };
+        ajout.onerror = function () {
+          alert('Erreur : ' + ajout.error);
+        };
+        this.criterions = [];
+        this.criterion = '';
+        this.nameTest = '';
+      }else{
+        this.formOk = false;
+      }
 
     },
+    addCriterion() {
+      if(this.criterion) {
+        this.formOkCriterion = true;
+        this.criterions.push(this.criterion)
+
+        this.criterion = ''
+      }else{
+        this.formOkCriterion = false;
+      }
+    },
+    removeCriterion(item){
+      this.criterions.splice(this.criterions.indexOf(item), 1)
+    }
 
   },
 
 
 }
+function serialize(value) {
+  if (typeof value === 'function') {
+    return value.toString();
+  }
+  if (typeof value === 'object') {
+    var serializeObject = {};
+    for (const [objectKey, objectValue] of Object.entries(value)) {
+      serializeObject[objectKey] = serialize(objectValue);
+    }
+    return serializeObject;
+  }
+
+  return value;
+}
+
 
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
 import CreateTest from "@/components/CreateTest";
-import HelloWorld from "@/components/HelloWorld";
+
 const routes = [
     // will match everything and put it under `$route.params.pathMatch`
     { path: '/administration', name: 'admin', component: CreateTest },
     // will match anything starting with `/user-` and put it under `$route.params.afterUser`
-    { path: '/', name: 'home', component: HelloWorld },
+
 ]
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
